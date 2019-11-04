84b405bfe37119fab6f725b989892829823168d7
lakesidetech <ramolo2009@gmail.com>
2019-11-04 17:51:46 +0300

    parameter passing in ruby

---
+++ my_file.rb
@@ -0,0 +1,19 @@
+def my_name
+    "ruth"
+end
+puts my_name
+
+def sum (a,b)
+a+b
+end
+puts sum(2,3)
+
+def age_difference (age_of_mother,age_of_daughter)
+    age_of_mother-age_of_daughter
+    end
+    puts age_difference(60,20)
+
+    def have_valid_id(x="No")
+         puts x
+            end
+    puts have_valid_id()
