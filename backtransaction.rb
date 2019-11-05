a211d680a156f01a7a72604da3e44ba58b70b9e2
lakesidetech <ramolo2009@gmail.com>
2019-11-05 15:08:23 +0300

    bank transactions

---
+++ backtransaction.rb
@@ -0,0 +1,28 @@
+#create an instance of class BankTransaction assigning it variable equity
+
+class BankTransaction
+
+    def just_arrived
+        puts "pick your ticket from the 
+        machine"
+    end
+
+    def have_picked_up_ticket
+        puts "sit and wait for your turn"
+    end
+
+    def have_valid_id?(response)
+        response = response.downcase
+        if response == "yes"
+            "transaction occurs will occur"
+        else
+            "no transaction"
+        end
+    end
+
+end
+
+bankTransaction1 = BankTransaction.new    
+puts bankTransaction1.just_arrived
+puts bankTransaction1.have_picked_up_ticket
+puts bankTransaction1.have_valid_id?("no")
