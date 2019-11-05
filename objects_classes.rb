b824dee8c6d6fd6ad229bf37c81fb53471a02d01
lakesidetech <ramolo2009@gmail.com>
2019-11-05 11:36:46 +0300

    My objects file

---
+++ objects_classes.rb
@@ -0,0 +1,15 @@
+#[10, 5, 10, 3, 100] to arrange elements inside it from smallest to largest
+
+ar=[10, 5, 10, 3, 100]
+    
+    puts ar.sort
+#ask 100 to increase itself by 5
+    
+x=100
+puts x++5
+
+#{"name" => "Jane Doe", "gender" => "female", "age" => 18} to give you all the keys it has and all the values it has
+
+details={"name" => "Jane Doe", "gender" => "female", "age" => 18} 
+puts details.keys
+puts details.values
